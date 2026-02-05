63ba6d559a3f43ad287af026ab9f4b8cd0df4681
yfussy <toainth@gmail.com>
2026-02-05 20:31:43 +0700

    checker: delete passed problem from order

--- checker/deleteTestcases.cpp
+++ checker/deleteTestcases.cpp
@@ -40,5 +40,44 @@ int main() {
         }
     }
 
-    cout << "Deleted " << deleted << " file(s)!";
+    cout << "Deleted " << deleted << " file(s)!" << endl;
+    
+    string orderName = "order.txt";
+    fs::path orderPath = fs::current_path().parent_path() / orderName;
+    if (!fs::exists(orderPath)) {
+        cerr << "Error: File not found at " << orderPath << endl;
+    }
+
+    fs::path tempPath = orderPath.parent_path() / (orderPath.stem().string() + "_temp" + orderPath.extension().string());
+
+    ifstream inFile(orderPath);
+    ofstream outFile(tempPath);
+
+    string line;
+    bool success = false;
+
+    if (inFile && outFile) {
+        while (getline(inFile, line)) {
+            if (line != testcase) outFile << line << endl;
+            else success = true;
+        }
+    }
+    
+    // close to enable delete/renaming file
+    inFile.close();
+    outFile.close();
+
+    if (success) {
+        try {
+            fs::remove(orderPath);
+            fs::rename(tempPath, orderPath);
+            cout << "Successfully deleted problem from order.txt!" << endl;
+        } catch (const fs::filesystem_error& e) {
+            cerr << "Filesystem Error: " << e.what() << endl;
+        }
+    } else {
+        fs::remove(tempPath);
+        cout << "There's no " << testcase << " to be deleted!" << endl;
+    }
+
 }
